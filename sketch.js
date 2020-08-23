872f536f84f120be771e3f70e6ba978eedc3c32d
= <=>
2020-08-23 13:52:51 +0100

    bug arse

--- sketch.js
+++ sketch.js
@@ -37,7 +37,7 @@ function keyPressed() {
 
   switch(keyCode) {
     case 32: //space bar
-      kick.triggerAttackRelease("C2", "1n");
+      synth.triggerAttackRelease("C2", "1n");
       break;
     case 8: //backspace
       synth.triggerAttackRelease(["B2","B3","B4","B5"], "1n");
@@ -72,16 +72,16 @@ function keyPressed() {
       synth.triggerAttackRelease("D#1", "1m");
       break;
     case 66: //b
-      synth2.triggerAttackRelease("F1", "1n");
+      synth.triggerAttackRelease("F1", "1n");
     	break;
     case 67: //c
-      synth2.triggerAttackRelease("A#1", "1n");
+      synth.triggerAttackRelease("A#1", "1n");
     	break;
     case 68: //d
       synth.triggerAttackRelease("C2", "1n");
     	break;
     case 69: //e
-      synth3.triggerAttackRelease("D#2", "1n");
+      synth.triggerAttackRelease("D#2", "1n");
     	break;
     case 70: //f  
       synth.triggerAttackRelease("F2", "1n");
@@ -93,7 +93,7 @@ function keyPressed() {
       synth.triggerAttackRelease("A#2", "1n");
       break;
     case 73: //i
-      synth3.triggerAttackRelease("C3", "1n");
+      synth.triggerAttackRelease("C3", "1n");
     	break;
     case 74: //j
       synth.triggerAttackRelease("F3", "1n");
@@ -105,46 +105,46 @@ function keyPressed() {
       synth.triggerAttackRelease("A#3", "1n");
     	break;
     case 77: //m
-      synth2.triggerAttackRelease("C4", "1n");
+      synth.triggerAttackRelease("C4", "1n");
     	break;
     case 78: //n
-      synth2.triggerAttackRelease("D#4", "1n");
+      synth.triggerAttackRelease("D#4", "1n");
     	break;
     case 79: //o
-      synth3.triggerAttackRelease("F4", "1n");
+      synth.triggerAttackRelease("F4", "1n");
     	break;
     case 80: //p
-      synth3.triggerAttackRelease("G#4", "1n");
+      synth.triggerAttackRelease("G#4", "1n");
     	break;
     case 81: //q
-      synth3.triggerAttackRelease("A#4", "1n");
+      synth.triggerAttackRelease("A#4", "1n");
     	break;
     case 82: //r
-      synth3.triggerAttackRelease("C5", "1n");
+      synth.triggerAttackRelease("C5", "1n");
     	break;
     case 83: //s
       synth.triggerAttackRelease("D#5", "1n");
     	break;
     case 84: //t
-      synth3.triggerAttackRelease("F5", "1n");
+      synth.triggerAttackRelease("F5", "1n");
     	break;
     case 85: //u
-      synth3.triggerAttackRelease("G#5", "1n");
+      synth.triggerAttackRelease("G#5", "1n");
     	break;
     case 86: //v
-      synth2.triggerAttackRelease("A#5", "1n");
+      synth.triggerAttackRelease("A#5", "1n");
     	break;
     case 87: //w
-      synth3.triggerAttackRelease("C6", "1n");
+      synth.triggerAttackRelease("C6", "1n");
     	break;
     case 88: //x
-      synth2.triggerAttackRelease("D#6", "1n");
+      synth.triggerAttackRelease("D#6", "1n");
     	break;
     case 89: //y
-      synth3.triggerAttackRelease("F6", "1n");
+      synth.triggerAttackRelease("F6", "1n");
     	break;
     case 90: //z
-      synth2.triggerAttackRelease("G#6", "1n");
+      synth.triggerAttackRelease("G#6", "1n");
     	break;
     default:
       break;
